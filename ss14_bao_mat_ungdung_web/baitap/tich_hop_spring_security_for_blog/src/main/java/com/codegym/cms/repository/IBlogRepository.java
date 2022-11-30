b0786e1b4771678be045980c8adff58f277de929
viettrieu123123 <viettrieu123123@gmail.com>
2022-11-30 10:12:14 +0700

    update

---
+++ ss14_bao_mat_ungdung_web/baitap/tich_hop_spring_security_for_blog/src/main/java/com/codegym/cms/repository/IBlogRepository.java
@@ -0,0 +1,22 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IBlogRepository extends JpaRepository<Blog,Long> {
+
+    @Query(value = "from Blog where tenBlog like :keyword")
+    List<Blog> searchName(@Param("keyword") String name);
+
+    Page<Blog> findAllByTenBlogContaining(String name, Pageable pageable);
+
+    Page<Blog> findAll(Pageable pageable);
+}
